d0a2568f8fc4d3009cfed36a9540d06af1d9d672
Beherit <4.blood@gmail.com>
2015-01-19 19:56:13 +0000

    Give the test some more time to run

--- packages/tests/test_models.py
+++ packages/tests/test_models.py
@@ -91,8 +91,8 @@ class TestSnapshot(DatabaseTestMixin, TestCase):
         self.db_session.flush()
 
         now = datetime.utcnow()
-        a_second_ago = now - timedelta(seconds=1)
-        a_second_in_the_future = now + timedelta(seconds=1)
+        a_second_ago = now - timedelta(seconds=10)
+        a_second_in_the_future = now + timedelta(seconds=10)
         self.assertGreater(snapshot.time, a_second_ago)
         self.assertGreater(a_second_in_the_future, snapshot.time)
 
