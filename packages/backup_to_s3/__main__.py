b0ab5c1d9cbd42071ac17732746c483f4a8e20ad
Beherit <4.blood@gmail.com>
2015-01-25 13:05:34 +0000

    Catch configuration errors on runtime

--- packages/backup_to_s3/__main__.py
+++ packages/backup_to_s3/__main__.py
@@ -7,7 +7,11 @@ sys.path.append(os.path.dirname(__file__))
 from snapshot.controller import SnapshotController
 from database.configuration_reader import DatabaseConfigurationReader
 from database.controller import DatabaseController
+from snapshot.exceptions import InvalidConfigurationError
 
-database_path = DatabaseConfigurationReader.get_path()
-DatabaseController.setup(database_path)
-SnapshotController.take_snapshot()
+try:
+    database_path = DatabaseConfigurationReader.get_path()
+    DatabaseController.setup(database_path)
+    SnapshotController.take_snapshot()
+except InvalidConfigurationError as e:
+    print("Invalid configuration: %s" % e)
