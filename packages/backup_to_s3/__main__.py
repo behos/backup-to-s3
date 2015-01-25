91f2a65f16f45fd0918c8ff930a385be1ae41b87
Beherit <4.blood@gmail.com>
2015-01-25 13:03:03 +0000

    Configure the database on run

--- packages/backup_to_s3/__main__.py
+++ packages/backup_to_s3/__main__.py
@@ -1,8 +1,13 @@
 import sys
+
 import os
 
 sys.path.append(os.path.dirname(__file__))
 
 from snapshot.controller import SnapshotController
+from database.configuration_reader import DatabaseConfigurationReader
+from database.controller import DatabaseController
 
+database_path = DatabaseConfigurationReader.get_path()
+DatabaseController.setup(database_path)
 SnapshotController.take_snapshot()
