e816935812419b4ac0e0c6de748618e121c74b4d
Beherit <4.blood@gmail.com>
2015-02-22 11:29:48 +0000

    Connect restorer to snapshot controller

--- packages/backup_to_s3/snapshot/controller.py
+++ packages/backup_to_s3/snapshot/controller.py
@@ -1,4 +1,5 @@
 from snapshot.configuration_reader import SnapshotConfigurationReader
+from snapshot.restorer import SnapshotRestorer
 from snapshot.taker import SnapshotTaker
 
 
@@ -14,3 +15,15 @@ class SnapshotController(object):
         )
 
         snapshot_taker.take_snapshot()
+
+    @staticmethod
+    def restore_snapshot(snapshot):
+        source = SnapshotConfigurationReader.get_destination()
+        destination = SnapshotConfigurationReader.get_source()
+        snapshot_restorer = SnapshotRestorer(
+            source_storage_device=source,
+            destination_storage_device=destination
+        )
+        snapshot_restorer.restore_snapshot(
+            snapshot=snapshot
+        )
--- packages/tests/snapshot/test_controller.py
+++ packages/tests/snapshot/test_controller.py
@@ -27,3 +27,30 @@ class TestSnapshotController(TestCase):
             destination_storage_device=mock_destination
         )
         assert mock_take_snapshot.called
+
+    def test_snapshot_controller_initialises_and_runs_snapshot_restorer(self):
+
+        mock_source = object()
+        mock_destination = object()
+        mock_snapshot = object()
+
+        with patch('snapshot.configuration_reader.'
+                   'SnapshotConfigurationReader.get_source',
+                   return_value=mock_source), \
+            patch('snapshot.configuration_reader.'
+                  'SnapshotConfigurationReader.get_destination',
+                  return_value=mock_destination), \
+            patch('snapshot.restorer.SnapshotRestorer.__init__',
+                  return_value=None) as snapshot_initialiser, \
+            patch('snapshot.restorer.SnapshotRestorer'
+                  '.restore_snapshot') as mock_restore_snapshot:
+                SnapshotController.restore_snapshot(mock_snapshot)
+
+        snapshot_initialiser.assert_called_with(
+            source_storage_device=mock_destination,
+            destination_storage_device=mock_source
+        )
+
+        mock_restore_snapshot.assert_called_with(
+            snapshot=mock_snapshot
+        )
