95b3dfeb03d0088e90e8cd19a9e16e3688111f3c
Beherit <4.blood@gmail.com>
2015-02-22 11:33:47 +0000

    Rename storage devices in snapshot controller

--- packages/backup_to_s3/snapshot/controller.py
+++ packages/backup_to_s3/snapshot/controller.py
@@ -7,23 +7,31 @@ class SnapshotController(object):
 
     @staticmethod
     def take_snapshot():
-        source = SnapshotConfigurationReader.get_source()
-        destination = SnapshotConfigurationReader.get_destination()
+        storage, backup_storage = \
+            SnapshotController._get_storage_devices_from_configuration()
+
         snapshot_taker = SnapshotTaker(
-            source_storage_device=source,
-            destination_storage_device=destination
+            source_storage_device=storage,
+            destination_storage_device=backup_storage
         )
 
         snapshot_taker.take_snapshot()
 
     @staticmethod
     def restore_snapshot(snapshot):
-        source = SnapshotConfigurationReader.get_destination()
-        destination = SnapshotConfigurationReader.get_source()
+        storage, backup_storage = \
+            SnapshotController._get_storage_devices_from_configuration()
+
         snapshot_restorer = SnapshotRestorer(
-            source_storage_device=source,
-            destination_storage_device=destination
+            source_storage_device=backup_storage,
+            destination_storage_device=storage
         )
         snapshot_restorer.restore_snapshot(
             snapshot=snapshot
         )
+
+    @staticmethod
+    def _get_storage_devices_from_configuration():
+        storage = SnapshotConfigurationReader.get_source()
+        backup_storage = SnapshotConfigurationReader.get_destination()
+        return storage, backup_storage
